5143320a03c54bc9d671f6b06e189977d623d012
VTramon <vitorr.ooliveira@gmail.com>
2023-08-24 08:53:54 -0300

    First commit

---
+++ src/test/java/dio/myfirstwebapp/MyFirstWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.myfirstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
